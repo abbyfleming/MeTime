ce454c575fd50eb97fb387047d7e441ac3ba7c5b
Abby Moore <abbyleighanne@gmail.com>
2016-12-15 22:52:00 -0600

    favorites show

--- app/controllers/itemFavCtrl.js
+++ app/controllers/itemFavCtrl.js
@@ -14,13 +14,39 @@ app.controller('ItemFavCtrl', function($scope, ItemFactory){
 			favoriteId.push(findId);
 		}
 
-		console.log(favoriteId);
-		//$scope.sortFavorites(favoriteArray);
+		//console.log(favoriteId);
+		$scope.sortFavorites(favoriteId);
 	}); 
 
 
-	// $scope.sortFavorites = (favoriteArray) => {
-	// console.log("favoriteArray", favoriteArray); 
-	// };
+	$scope.sortFavorites = (favoriteId) => {
+		console.log("favoriteArrayNumbers", favoriteId); 
+		let favoriteArray = [];
+
+		for (var i = 0; i < favoriteId.length; i++){
+			let id = favoriteId[i];
+			
+			ItemFactory.singleFavorite(id)
+			.then((favoriteData) => {
+				//console.log(favoriteData);
+				favoriteArray.push(favoriteData);
+				console.log("fav array", favoriteArray);
+			});
+
+		$scope.items = favoriteArray;
+		$scope.$apply();
+		}
+	};
+
+
+
+
+		// 	$scope.showFavorites()
+		// 	.then( (itemArray) => {
+		// 	console.log("factory", itemArray);
+		// 	$scope.items = itemArray;
+		// 	$scope.$apply();
+		// });
+
 
 });
